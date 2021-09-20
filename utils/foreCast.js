30a6f6df935aad437cf42020f2bb6c4da4bd4faa
Joshua Israel <joshua.i@e360africa.com>
2021-09-20 03:21:30 +0100

    Done with some edit

--- utils/foreCast.js
+++ utils/foreCast.js
@@ -4,15 +4,15 @@ const forecast = (latitude,longitude, callback) => {
   
     const url = "http://api.weatherstack.com/current?access_key=e84129bc20b99e3b1452c36379b4e3ff&query=" +latitude+','+longitude
 
-        request({ url: url, json: true }, (error, response) => {
+        request({ url: url, json: true }, (error, {body}) => {
             if(error){
                 callback('unable to connect to the forecast', undefined);
-            }else if(response.body.error){
+            }else if(body.error){
                 callback('unable to locate weather condition services', undefined);
             }else{
                 callback(undefined, {
-                    description: response.body.current.weather_descriptions[0],
-                    temperature: response.body.current.temperature
+                    description: body.current.weather_descriptions[0],
+                    temperature: body.current.temperature
                 });
             }
         })
